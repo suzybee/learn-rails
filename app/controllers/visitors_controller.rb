9b34efa9e5b4139b64f261cd11276b8e3b8d77fa
Suzy Birmingham <suzy@suzybirmingham.com>
2014-11-01 17:18:33 -0400

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birthday is soon.'
 		## Rails API will do the following function: render 'visitors/new' (plus more black magic)
 	end
 
