606c1e3b995af833c486c476c00203277ce28ea5
Suzy Birmingham <suzy@suzybirmingham.com>
2014-11-05 13:41:49 -0500

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
-		flash.now[:notice] = 'Welcome!'
-		flash.now[:alert] = 'My birthday is soon.'
 		## Rails API will do the following function: render 'visitors/new' (plus more black magic)
 	end
 
